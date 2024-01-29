563e2b53b728f220c2cce1a42cbd8d65274e41c5
revus01 <qoalsgks9@gmail.com>
2024-01-29 20:04:24 +0900

    사격 판정 및 스프레드 개선
    - 사격 판정 시스템 조정
    - 스프레드 적용 단계 변경

--- Assets/Nikke/codes/GunSystem.cs
+++ Assets/Nikke/codes/GunSystem.cs
@@ -23,7 +23,7 @@ public class GunSystem : MonoBehaviour
     public Transform gunModel;
     public Transform attackPoint;
     public Transform pivotPoint;
-    public RaycastHit rayHit;
+    public RaycastHit rayHit, spreadHit;
     public LayerMask whattIsEnemy;
 
     // Graphics
@@ -59,40 +59,47 @@ public class GunSystem : MonoBehaviour
     {
         readyToShoot = false;
 
-        // 원형 spread
-        float angle = Random.Range(0f, 2f * Mathf.PI); // 0에서 2파이(360도) 사이의 무작위 각도를 선택합니다.
-        float radius = Random.Range(0f, spread); // 0에서 spread 사이의 무작위 반지름을 선택합니다.
 
-        float x = radius * Mathf.Cos(angle);
-        float y = radius * Mathf.Sin(angle);
-
-
-
-        // 마우스 위치 + 스프레드 보정
-        Ray ray = tpsCam.ScreenPointToRay(Input.mousePosition + new Vector3(x, y, 0));
+        // 마우스 위치
+        Ray ray = tpsCam.ScreenPointToRay(Input.mousePosition);
 
         // RayCast 마우스가 보고 있는 물체에 바로 쏘아줌
         if (Physics.Raycast(ray, out rayHit, range, whattIsEnemy))
         {
 
-            // 총 회전
+            // 총 회전 - rayHit를 사용하는 이유는 스프레드가 직접적으로 총규 방향을 결정짓는 것은 아니기 때문임. 같은 곳을 보고 쏜다면 같은 곳을 가리켜야 함.
             Vector3 pivotToHit = rayHit.point - pivotPoint.position;
             Vector3 newDirection = pivotToHit.normalized;
             gunModel.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
 
+            // 원형 spread
+            float angle = Random.Range(0f, 2f * Mathf.PI); // 0에서 2파이(360도) 사이의 무작위 각도를 선택합니다.
+            float radius = Random.Range(0f, spread); // 0에서 spread 사이의 무작위 반지름을 선택합니다.
 
-            Debug.Log(rayHit.collider.name);
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
 
-            if (rayHit.collider.CompareTag("Enemy"))
+            // 스프레드 적용된 탄착점
+            Ray spreadRay = tpsCam.ScreenPointToRay(Input.mousePosition + new Vector3(x, y, 0));
+            Physics.Raycast(spreadRay, out spreadHit, range);
+
+
+
+            Debug.Log(spreadHit.collider.name);
+
+            TrailRenderer trail = Instantiate(bulletTrail, attackPoint.position, Quaternion.identity);
+            StartCoroutine(SpawnTrail(trail, spreadHit));
+            GameObject instantiatedMuzzleFlash = Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+
+            // 적에게 공격이 닿았을 때만 데미지를 줌.
+            if (spreadHit.collider.CompareTag("Enemy"))
             {
-                TrailRenderer trail = Instantiate(bulletTrail, attackPoint.position, Quaternion.identity);
-                StartCoroutine(SpawnTrail(trail, rayHit));
-                rayHit.collider.GetComponent<EnemyAction>().TakeDamage(damage);
-                GameObject instantiatedMuzzleFlash = Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
-
-                bulletsLeft--;
-                Destroy(instantiatedMuzzleFlash, 0.1f);
+                spreadHit.collider.GetComponent<EnemyAction>().TakeDamage(damage);
             }
+
+            bulletsLeft--;
+            Destroy(instantiatedMuzzleFlash, 0.1f);
 
             // Debug.DrawRay(attackPoint.position, rayHit.point - attackPoint.position, Color.red, 20f);
 
